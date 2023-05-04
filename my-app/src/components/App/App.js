4372dcf8990de08e8d2a0059602ed9e20c3d7142
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-05-04 15:31:37 +0100

    Took our states not used

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import HomePage from "../HomePage";
 import MyFlashcards from "../MyFlashcards";
--- my-app/src/components/HomePage/index.js
+++ my-app/src/components/HomePage/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import "../App/App.css";
 import bearTitleImage from "../../assets/bearTitleImage.png";
 import RandomFlashCard from "../RandomFlashCard";
